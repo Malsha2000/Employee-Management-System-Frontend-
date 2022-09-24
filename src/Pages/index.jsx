e864834ef3e3ad13745bc20bf39fdf2153eb8573
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-09-24 14:41:24 +0530

    Import pages

---
+++ src/Pages/index.jsx
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import LoginForm from "./Login";
+import AddEmployee from "./Employee/Add/index";
+import UpdateEmployee from "./Employee/Update/index";
+import EmployeeList from "./Employee/List/index";
+import AboutUsPage from "./AboutUs/index";
+
+
+export {
+    
+    Home,
+    LoginForm,
+    AddEmployee,
+    UpdateEmployee,
+    EmployeeList,
+    AboutUsPage,
+};
