167634cd68f7f0070b67a4e278d1288c9759b517
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-09-24 14:43:54 +0530

    Define routes

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
+import AboutUs from '../Components/AboutUs'
+import Login from '../Components/LoginForm'
+import AddEmployee from '../Pages/Employee/Add/index'
+import UpdateEmployee from '../Pages/Employee/Update/index'
+import EmployeeList from '../Pages/Employee/List/index'
 
 const PageRoutes = () => {
 
@@ -8,7 +13,11 @@ const PageRoutes = () => {
     <div>
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home/>}></Route>
+                <Route path='/' element={<Login/>}></Route>
+                <Route path='/aboutus' element={<AboutUs/>}></Route>
+                <Route path='/employee/add' element={<AddEmployee/>}></Route>
+                <Route path='/employee/update/:id' element={<UpdateEmployee/>}></Route>
+                <Route path='/employee/all' element={<EmployeeList/>}></Route>
             </Routes>
         </BrowserRouter>
     </div>
