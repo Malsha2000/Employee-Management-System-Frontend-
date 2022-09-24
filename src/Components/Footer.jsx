06ff3d01eb5686e49011ab5c304fdbc9b3f9c6f9
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-09-24 15:15:36 +0530

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,13 +6,8 @@ const Footer = () => {
 			<div class="md:flex md:justify-between">
 				<div class="mb-6 md:mb-0">
 					<a
-						href="https://flowbite.com/"
-						class="flex items-center">
-						<img
-							src="https://flowbite.com/docs/images/logo.svg"
-							class="mr-3 h-8"
-							alt="FlowBite Logo"
-						/>
+						href=""
+						class="flex items-center">					
 						<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
 							CITY J7
 						</span>
